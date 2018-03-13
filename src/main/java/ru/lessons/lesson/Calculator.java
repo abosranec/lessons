7884c4ee1c52778b7af2f2ba7beeaa523eefd476
pasha <pasha_mugako@mail.ru>
2018-03-13 12:34:00 +0300

    add tests

--- src/main/java/ru/lessons/lesson/Calculator.java
+++ src/main/java/ru/lessons/lesson/Calculator.java
@@ -1,8 +1,5 @@
 package ru.lessons.lesson;
 
-/**
- * Created by VitAl on 13.03.2018.
- */
 public class Calculator {
 
     private double result = 0;
@@ -22,15 +19,17 @@ public class Calculator {
      * @param params - arguments for multiply
      */
     public void mul(double... params){
+        double resultMul = 1;
         for (double param: params){
-            result *= param;
+            resultMul *= param;
         }
+        result = resultMul;
     }
 
     /**
      * divite numbers
-     * @param param1
-     * @param param2
+     * @param param1 param for divide
+     * @param param2 param for divide
      */
     public void div(double param1, double param2){
         result = param1 / param2;
@@ -38,8 +37,8 @@ public class Calculator {
 
     /**
      * subtract numbers
-     * @param param1
-     * @param param2
+     * @param param1 param for subtract
+     * @param param2 param for subtract
      */
     public void sub(double param1, double param2){
         result = param1 - param2;
---
+++ src/test/java/ru/lessons/lesson/CalculatorTest.java
@@ -0,0 +1,36 @@
+package ru.lessons.lesson;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    @Test
+    public void add() throws Exception {
+        Calculator calc = new Calculator();
+        calc.add(1, 1.05);
+        Assert.assertEquals(2, calc.getResult(), 0.1);
+    }
+
+    @Test
+    public void div() throws Exception {
+        Calculator calc = new Calculator();
+        calc.div(5, 2);
+        Assert.assertEquals(2.5, calc.getResult(), 0.1);
+    }
+
+    @Test
+    public void sub() throws Exception {
+        Calculator calc = new Calculator();
+        calc.sub(4, 2);
+        Assert.assertEquals(2, calc.getResult(), 0.1);
+    }
+
+    @Test
+    public void mul() throws Exception {
+        Calculator calc = new Calculator();
+        calc.mul(3, 6);
+        Assert.assertEquals(18, calc.getResult(), 0.1);
+    }
+}
