1dd92932e506ae3a8eaf4b663bd1e30f69afac1e
pasha <pasha_mugako@mail.ru>
2019-02-26 00:08:17 +0300

    change equals

--- src/main/java/petsClinic/Client.java
+++ src/main/java/petsClinic/Client.java
@@ -76,6 +76,12 @@ public class Client {
 
     @Override
     public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
         return (name.equals(((Client)obj).getName()));
     }
 
--- src/main/java/petsClinic/pets/Pet.java
+++ src/main/java/petsClinic/pets/Pet.java
@@ -35,6 +35,12 @@ public abstract class Pet {
 
     @Override
     public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
         return (name.equals(((Pet)obj).getName()) && type.equals(((Pet)obj).getType()));
     }
 
