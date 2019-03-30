6c136d4532c89a0c5c2a478d56e03d142dcae91b
pasha <pasha_mugako@mail.ru>
2019-03-31 02:45:30 +0300

    get connection with database, start to remake petClinic

--- clinic-pet-web/src/main/java/petsClinic/TestJDBC.java
+++ clinic-pet-web/src/main/java/petsClinic/TestJDBC.java
@@ -1,7 +1,40 @@
 package petsClinic;
 
+import petsClinic.service.Settings;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestJDBC {
     public static void main(String[] args) {
+        Settings settings = Settings.getInstance();
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(settings.value("jdbc.url"), settings.value("jdbc.username"), settings.value("jdbc.password"));
+            List<Client> list = new ArrayList<Client>();
+            try(final Statement statement = connection.createStatement();
+                final ResultSet rs = statement.executeQuery("select * from Clients;")){
+                while(rs.next()){
+                    list.add(new Client(rs.getString("name")));
+                }
+                for (Client client: list) {
+                    System.out.println(client.getName());
+                }
+            }
 
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
