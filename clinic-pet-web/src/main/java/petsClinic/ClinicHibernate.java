9ad8728358c9142dd7dd497ac1340141943f5336
pasha <pasha_mugako@mail.ru>
2019-04-08 17:26:05 +0300

    still find error...

--- clinic-pet-web/src/main/java/petsClinic/ClinicHibernate.java
+++ clinic-pet-web/src/main/java/petsClinic/ClinicHibernate.java
@@ -3,7 +3,9 @@ package petsClinic;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 import petsClinic.service.Settings;
 
 import java.sql.*;
@@ -11,28 +13,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClinicHibernate implements ClinicStorage {
-    private final SessionFactory factory;
-    private final Settings settings = Settings.getInstance();
+//    private ;
 
     public ClinicHibernate() {
-        factory = new Configuration().configure().buildSessionFactory();
-        try {
-            Class.forName(settings.value("jdbc.driver_class"));
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        System.out.println("\nbefore constructor\n");
+//        try {
+//            factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+        System.out.println("\nafter constructor\n");
     }
 
     @Override
     public List<Client> getClients() {
-        final Session session = factory.openSession();
+        List<Client> list = null;
+        SessionFactory factory = null;
+
+//        Configuration configuration = new Configuration().configure();
+//        ServiceRegistry serviceRegistry
+//                = new StandardServiceRegistryBuilder()
+//                .applySettings(configuration.getProperties()).build();
+//
+//        // builds a session factory from the service registry
+//        SessionFactory factory = configuration.buildSessionFactory(serviceRegistry);
+
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Session session = factory.openSession();
         Transaction tx = session.beginTransaction();
         try {
-            return session.createQuery("from Client ").list();
-        } finally {
+            list = session.createQuery("from Client ").list();
+        } catch (Exception e){
+            e.printStackTrace();
+        }finally {
             tx.commit();
             session.close();
         }
+        if (list == null){
+            System.out.println("Don't have list of clients");
+        }
+        return list;
     }
 
     @Override
---
+++ clinic-pet-web/src/test/java/petsClinic/servlets/ClientViewServletTest.java
@@ -0,0 +1,16 @@
+package petsClinic.servlets;
+
+import org.junit.Test;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class ClientViewServletTest {
+
+    @Test
+    public void doGet() {
+
+    }
+}
